ccc60b459c03f8b57602e963bc3dcf8f7f949bac
Desislava Milusheva <deisi_deisi@abv.bg>
2020-06-26 15:17:37 +0300

    Last commit

---
+++ Stage_2/StoreManagmentSystem/StoreManagmentSystem/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StoreManagmentSystem
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(string id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public virtual string ToStringPerson()
+        {
+            return string.Format("{0}, {1}", Id, Name);
+        }
+    }
+}
